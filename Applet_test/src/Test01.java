4eb8cefa5b34cdff31d73048d60c90a35d9a56d4
masayoshi2 <masayoshi2@masa-SF>
2014-08-06 15:32:24 +0900

    init

---
+++ Applet_test/src/Test01.java
@@ -0,0 +1,29 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+
+public class Test01 extends Applet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		super.init();
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		// TODO Auto-generated method stub
+		g.drawString("•¶Žš‚ð•\Ž¦",10,10);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
